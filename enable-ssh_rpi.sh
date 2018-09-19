ca18ca391f4aec23bfc2049a181914ae960dae1c
Distant-One <kmarr41@gmail.com>
2018-09-19 16:38:42 -0400

    Changed teh to the in comments

--- enable-ssh_rpi.sh
+++ enable-ssh_rpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # enable_ssh_rpi.sh
-# add a file to /boot directory of teh rpi image named ssh to enable ssh
+# add a file to /boot directory of the rpi image named ssh to enable ssh
 #
 # usage: enable_ssh_rpi.sh mountpoint
 # rpi image must already be mounted
